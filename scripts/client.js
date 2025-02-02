81810b73c4bb7f7adf1966773718cddc44eb6d95
Aaron-Meier-Student <aamea72@student.oaisd.org>
2025-02-02 17:42:00 -0500

    Changed how the side loads, made styles load like scripts.

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,20 @@
 const money = new Money();
+const userData = [
+    // ["Student Name", ["Variant0", "Variant1"], ["Pinned"]]
+];
 
-tooltip({dom: document.getElementById("signin"), text: "Signing in lets you save your progress!", delay: 0})
-tooltip({dom: document.querySelector("#topbar > h2:nth-child(1)"), text: "WHAT THE ∑", delay: 10})
+function determineValue(card) {
+    let price = 0;
+    return price;
+}
+
+tooltip({
+    dom: document.getElementById("signin"),
+    text: "Signing in lets you save your progress!",
+    delay: 0,
+});
+tooltip({
+    dom: document.querySelector("#topbar > h2:nth-child(1)"),
+    text: "WHAT THE ∑",
+    delay: 10,
+});
--- scripts/system.js
+++ scripts/system.js
@@ -10,34 +10,49 @@ const components = [
 
 const utils = ["version", "supabase"];
 const scripts = ["client", "tabs"];
+const assets = ["cards", "variants", "packs"];
+const styles = ["main", "topbar"]
 
-function loadScript(dir, component) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement("script");
-        script.src = `${dir}/${component}.js`;
-        script.async = true;
-
-        script.onload = resolve;
-        script.onerror = reject;
-
-        document.head.appendChild(script);
+function deepFreeze(obj) {
+    Object.freeze(obj);
+    Object.keys(obj).forEach((key) => {
+        if (typeof obj[key] === "object" && obj[key] !== null) {
+            deepFreeze(obj[key]);
+        }
     });
 }
 
-async function loadScripts(dir, files) {
+function loadFile(dir, component, type) {
+    return new Promise((resolve, reject) => {
+        const tag = document.createElement(type);
+        tag[type == "script" ? "src" : "href"] = `./${dir}/${component}.${type == "script" ? "js" : "css"}`;
+        if (type == "link") tag.rel = "stylesheet";
+        tag.async = true;
+
+        tag.onload = resolve;
+        tag.onerror = reject;
+
+        document.head.appendChild(tag);
+    });
+}
+
+async function loadFiles(dir, files, type = "script") {
     try {
         const scriptPromises = files.map((component) =>
-            loadScript(dir, component)
+            loadFile(dir, component, type)
         );
         await Promise.all(scriptPromises);
     } catch (error) {
         console.error("Error loading components!", error);
     }
 }
+
 async function loadAll() {
-    await loadScripts("components", components);
-    await loadScripts("utils", utils);
-    await loadScripts("scripts", scripts);
+    await loadFiles("styles", styles, "link");
+    await loadFiles("components", components);
+    await loadFiles("utils", utils);
+    await loadFiles("scripts", scripts);
+    await loadFiles("assets", assets);
 }
 
 loadAll();
